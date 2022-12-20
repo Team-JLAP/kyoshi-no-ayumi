e68365bdaa609ee69935e20b4f523360fa05e439
Takeshi Hashimoto <htakeshi0614@gmail.com>
2022-12-20 00:09:39 -0800

    add environment variables

--- ayumi/settings.py
+++ ayumi/settings.py
@@ -11,21 +11,23 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import environ
 
+env = environ.Env()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+env.read_env(os.path.join(BASE_DIR,'.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^se(bjz@np#b_2%qtl==la-%o@3r=-ey%_*im1u_wte@9fz9by'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1','ayumi-environment.eba-3adymxpb.us-west-2.elasticbeanstalk.com', 'kyoshinoayumi.com']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 # Application definition
